08c9b4f861c001e58d85391374629119fea9e61e
Mohamed KADI <handskadi@gmail.com>
2023-07-24 11:45:44 +0000

    Task 2 : init project`

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - select sort algo
+ * @array: array to be sorted
+ * @size: array size
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	int aux = 0;
+	size_t i = 0, j = 0, pos = 0;
+
+	if (array == NULL || size == 0)
+		return;
+	for (; i < size - 1; i++)
+	{
+		pos = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[pos])
+				pos = j;
+		}
+		if (pos != i)
+		{
+			aux = array[i];
+			array[i] = array[pos];
+			array[pos] = aux;
+			print_array(array, size);
+		}
+	}
+}
